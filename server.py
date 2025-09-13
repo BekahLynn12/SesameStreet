bd1d6e1f51ba9a2da38490ae6c768a79dd768eb0
BekahLynn12 <149525740+BekahLynn12@users.noreply.github.com>
2025-09-13 01:41:00 +0000

    start

--- server.py
+++ server.py
@@ -29,9 +29,15 @@ for line in data_file:
 
     
 #opens index (home page)
-@app.route("/", methods=["GET", "POST"])
-def home():
-  return render_template("index.html")
+
+
+@app.get('/', function(req, res){
+  res.render('index.html');
+});
+
+#@app.route("/", methods=["GET", "POST"])
+#def home():
+#  return render_template("index.html")
 
 
 
